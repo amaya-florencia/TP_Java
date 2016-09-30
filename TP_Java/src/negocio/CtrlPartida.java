94070b34863d4120c20846993d21907fc30b0c04
amaya-florencia <amaya-florencia@outlook.com>
2016-09-30 17:40:33 -0300

    .

---
+++ TP_Java/src/negocio/CtrlPartida.java
@@ -0,0 +1,11 @@
+package negocio;
+
+import java.util.ArrayList;
+import entidades.*;
+import java.io.InvalidClassException;
+import util.ApplicationException;
+import data.DataPersonaje;
+
+public class CtrlPartida {
+
+}
--- TP_Java/src/util/ApplicationException.java
+++ TP_Java/src/util/ApplicationException.java
@@ -1,11 +1,11 @@
 package util;
 
-import org.apache.log4j.Level;
+import org.apache.logging.log4j.Level;
 
 public class ApplicationException extends Exception {
 
 	/**
-	 *  
+	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 
@@ -16,13 +16,12 @@ public class ApplicationException extends Exception {
 	
 	public ApplicationException(String message){
 		super(message);
-		SuperLogger.logger.log(null, message, Level.ERROR, message);
-		
+		SuperLogger.logger.log(Level.ERROR, message);
 	}
 	
 	public ApplicationException(String message, Throwable cause){
 		super(message,cause);
-		SuperLogger.logger.log(null, message, Level.ERROR, message);
+		SuperLogger.logger.log(Level.ERROR, message, cause);
 		
 	}
 }
