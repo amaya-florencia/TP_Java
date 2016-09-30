054d0bbdc5eb60c73571687e397d698d330d25f2
virtual <virtual@10.0.2.15>
2016-09-30 20:18:33 -0300

    ...8.18

--- TP_Java/src/presentacion/Principal.java
+++ TP_Java/src/presentacion/Principal.java
@@ -14,6 +14,7 @@ import javax.swing.JInternalFrame;
 
 public class Principal {
 
+	//http://www.java2s.com/Tutorial/Java/0240__Swing/JComboBoxUsingtheCustomModel.htm
 	private JFrame frame;
 
 	/**
